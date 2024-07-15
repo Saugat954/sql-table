fb387ed4963e0ae8f56929c8dccfda0426ef24df
Saugat954 <156522705+Saugat954@users.noreply.github.com>
2024-07-15 22:46:02 +1000

    Add files via upload

---
+++ create a table.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE college;
+USE college;
+CREATE TABLE student (
+rollno INT PRIMARY KEY,
+name VARCHAR(50)
+);
+INSERT INTO student (
+rollno, Name)
+values(23,'SAUGAT'),
+(25,'Subedi');
+
+INSERT INTO student (
+rollno, Name)
+values(28,'BIJAYA');
+
+SELECT * FROM student;
+
+
